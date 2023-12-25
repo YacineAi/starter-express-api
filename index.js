09586b465d1df51adf658d7363a18c7368650b65
Yacine Djenidi <djenidi.dev@gmail.com>
2023-12-25 11:09:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({});
+
+const proxyServer = http.createServer((req, res) => {
+  // Proxy the incoming request to the target server
+  proxy.web(req, res, {
+    target: 'http://apim.djezzy.dz', // Replace with the target server URL
+    changeOrigin: true
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+
+proxyServer.listen(PORT, () => {
+  console.log(`Proxy server listening on port ${PORT}`);
+});
