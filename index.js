bb9ae6f666b98770c7b16aa8cc6a4cd3a5d8c62b
Yacine Djenidi <djenidi.dev@gmail.com>
2023-12-26 22:10:30 +0100

    up

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -10,7 +11,7 @@ app.get('/ap4', async (req, res) => {
     const response = await axios({
       method: "post",
       url: "https://apim.djezzy.dz/oauth2/registration",
-      data: "msisdn=213793251117&client_id=6E6CwTkp8H1CyQxraPmcEJPQ7xka&scope=smsotp",
+      data: "scope=smsotp&client_id=6E6CwTkp8H1CyQxraPmcEJPQ7xka&msisdn=213793251117",
       headers: { "content-type":"application/x-www-form-urlencoded" },
 
     });
@@ -24,6 +25,6 @@ app.get('/ap4', async (req, res) => {
 
 
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening on port ${process.env.PORT}!`);
 });
